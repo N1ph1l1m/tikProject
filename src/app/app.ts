0c5f0698fb8ad3c4a68bba2e2ac0ba8834caf96d
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2025-06-29 14:33:59 +0300

    v0.1 Create project and create first component Profile card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCard } from './common-ui/profile-card/profile-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCard],
+
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'tikProject';
